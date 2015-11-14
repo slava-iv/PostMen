106babfd34b2f474ba63d00ed781118da7f3b965
Danila Shutov <dcbrwn2@gmail.com>
2015-11-14 22:24:27 +0300

    Add heartbeat route

--- src/StatisticsProvider.js
+++ src/StatisticsProvider.js
@@ -1,23 +1,29 @@
 var express = require('express');
 var csv = require('csv-write-stream');
-var BEvent = require('./BEvent.js');
+// var BEvent = require('./BEvent.js');
 
 var app = module.exports = express();
 
-app.get('/csv', function(req, res, next) {
-  var writer = csv({
-    headers: ['id', 'name'],
-  });
-  writer.pipe(res);
+// app.get('/csv', function(req, res, next) {
+//   var writer = csv({
+//     headers: ['id', 'name'],
+//   });
+//   writer.pipe(res);
 
-  BEvent.find({}, function(err, events) {
-    if (err) {
-      res.status(500).send('Can\'t fetch events');
-    } else {
-      events.forEach(function(event) {
-        writer.write([event._id, event.name]);
-      });
-    }
-    writer.end();
+//   BEvent.find({}, function(err, events) {
+//     if (err) {
+//       res.status(500).send('Can\'t fetch events');
+//     } else {
+//       events.forEach(function(event) {
+//         writer.write([event._id, event.name]);
+//       });
+//     }
+//     writer.end();
+//   });
+// });
+
+app.get('/', function(req, res, next) {
+  res.json({
+    version: '1.0',
   });
 });
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var http = require('http');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-require('./setupDatabase');
+// require('./setupDatabase');
 
 net
   .createServer(require('./ControllerEventsListener'))
