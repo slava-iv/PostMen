13e75b2fdd3f105d4aacf8d1a0af1c5428dac33f
Danila Shutov <dcbrwn2@gmail.com>
2015-11-14 22:09:43 +0300

    Add random position sender

--- src/GameEventsProvider.js
+++ src/GameEventsProvider.js
@@ -4,6 +4,7 @@ var clients = [];
 
 module.exports = function (socket) {
   socket.name = socket.remoteAddress + ":" + socket.remotePort 
+  console.log(socket.name);
 
   clients.push(socket);
 
@@ -11,7 +12,16 @@ module.exports = function (socket) {
     socket.write(data);
   });
 
+  var timer = setInterval(function() {
+  	socket.write('positionUpdated ' + Math.floor(Math.random() * 100) + '\n');
+  }, 1000 / 30);
+
+  socket.on('data', function(data) {
+    console.log(data.toString());
+  });
+
   socket.on('end', function () {
     clients.splice(clients.indexOf(socket), 1);
+    clearInterval(timer);
   });
 };
