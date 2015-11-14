5ae03105abc8710401da2cd3fe3d961094093a8f
Danila Shutov <dcbrwn2@gmail.com>
2015-11-14 15:14:38 +0300

    Cleanup unnecessary comments

--- src/ControllerEventsListener.js
+++ src/ControllerEventsListener.js
@@ -1,24 +1,18 @@
 var bus = require('./EventsBus.js');
 
-// Keep track of the chat clients
 var clients = [];
 
 module.exports = function (socket) {
-
-  // Identify this client
   socket.name = socket.remoteAddress + ":" + socket.remotePort 
 
-  // Put this new client in the list
   clients.push(socket);
 
-  // Handle incoming messages from clients.
   socket.on('data', function (data) {
-    console.log(data.toJSON());
+    console.log('>', data.toJSON());
+    bus.emit('throw', data);
   });
 
-  // Remove the client from the list when it leaves
   socket.on('end', function () {
     clients.splice(clients.indexOf(socket), 1);
   });
-
 };
--- src/GameEventsProvider.js
+++ src/GameEventsProvider.js
@@ -1,24 +1,17 @@
 var bus = require('./EventsBus.js');
 
-// Keep track of the chat clients
 var clients = [];
 
 module.exports = function (socket) {
-
-  // Identify this client
   socket.name = socket.remoteAddress + ":" + socket.remotePort 
 
-  // Put this new client in the list
   clients.push(socket);
 
-  // Handle incoming messages from clients.
-  socket.on('data', function (data) {
-    console.log(data.toJSON());
+  bus.on('throw', function(data) {
+    socket.write(data);
   });
 
-  // Remove the client from the list when it leaves
   socket.on('end', function () {
     clients.splice(clients.indexOf(socket), 1);
   });
-
 };
