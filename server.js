aae5ae4a54cf2ba1439e47d1912c834d0c9fc227
Danila Shutov <dcbrwn2@gmail.com>
2015-11-15 03:48:15 +0300

    Test server

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-var net = require('net');
+// var net = require('net');
 
-require('./src/setupDatabase');
+// require('./src/setupDatabase');
 
-net
-  .createServer(require('./src/RequestController'))
-  .listen(process.env.PORT || 8080, function() {
-    console.log('Statistics provider is ready on 8080');
-  });
+// net
+//   .createServer(require('./src/RequestController'))
+//   .listen(process.env.PORT || 8080, function() {
+//     console.log('Statistics provider is ready on 8080');
+//   });
+
+var express = require('express');
+
+var app = express();
+
+app.use('/', function(req, res) {
+  res.json({ test: 1 });
+});
+
+app.listen(process.env.PORT || 8080);
